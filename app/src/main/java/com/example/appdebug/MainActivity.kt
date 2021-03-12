586ef2a15302746ae21b8e8f8dc36ae960739f83
RFelixRocha <felixrochaeso@gmail.com>
2021-03-11 21:16:26 -0400

    Add try catch ao teste de tratamento de error

--- app/src/main/java/com/example/appdebug/MainActivity.kt
+++ app/src/main/java/com/example/appdebug/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.lang.IndexOutOfBoundsException
 
 class MainActivity : AppCompatActivity() {
     private var click = 0
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setLogListener()
+        val currentThread = Thread.currentThread()
     }
 
     private fun setLogListener(){
@@ -19,8 +22,16 @@ class MainActivity : AppCompatActivity() {
             Log.d("click","Opa! cliquei no debug")
         }
         bt_error?.setOnClickListener {
-            click++;
-            Log.e("click","Opa! cliquei no error")
+//            click++;
+//            Log.e("click","Opa! Houve $click clique no botão.")
+            try {
+
+                val list = listOf<Int>(2,4,6)
+                val a = list[6]
+
+            } catch (e: IndexOutOfBoundsException){
+                val x = 0
+            }
         }
         bt_info.setOnClickListener { Log.i("click","Opa! cliquei no info") }
         bt_warning.setOnClickListener { Log.w("click","Opa! cliquei no warning") }
